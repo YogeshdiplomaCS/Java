fc3846a43f05526a859066a945c9e3d0adb0f2dc
Yogesh <Yogeshdiploma@outlook.com>
2026-02-17 21:25:06 +0530

    Implement Fibonacci series in Fibbonacci.java
    This program prompts the user for a number and prints the Fibonacci series up to that number.

---
+++ Fibbonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Fibbonacci 
+    {
+        public static void main(String [] args)
+        {
+            int n, a=0, b=0, c=1;
+            Scanner s=new Scanner(System.in);
+            System.out.println("Enter value of n:");
+            n=s.nextInt();
+            System.out.println("Fibbanacci series:");
+            for(int i=1; i<=n; i++)
+            {
+                a=b;
+                b=c;
+                c=a+b;
+                System.out.print(a+ " ");
+            }
+                
+      }             
+    
+}
