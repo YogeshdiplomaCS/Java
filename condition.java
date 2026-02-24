6e79120551244e4a9ea965ce087875c935561e61
Yogesh <Yogeshdiploma@outlook.com>
2026-02-24 20:07:32 +0530

    Add simple if condition example in Java

---
+++ condition.java
@@ -0,0 +1,13 @@
+//simple if condition on java
+public class condition
+{
+    public static void main(String args[])
+    {
+        int a=200;
+        int b=300;
+        int c=400;
+        if (a>b || a>c)
+        {
+            System.out.println("At least one condition is true");
+        }
+    }
