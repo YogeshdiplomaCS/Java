4a935f7d043505013b00b8a63f6950885fca2ffc
Yogesh <Yogeshdiploma@outlook.com>
2026-02-24 21:16:16 +0530

    Add Rain class with if statement example

---
+++ Rain.java
@@ -0,0 +1,16 @@
+//Example code of ifstatement
+public class Rain
+{
+    public static void main(String args [])
+    {
+        boolean isRaining=true;
+        if(isRaining)
+        {
+          System.out.println("I am outside if , not a part of if!");
+          
+        }
+        else
+        {
+            System.out.println("No rain today , no need of an umbrella!");
+        }
+    }
