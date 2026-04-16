134206ecca63a08700c7649f385b9ae467e2f8c3
Yogesh <Yogeshdiploma@outlook.com>
2026-04-16 10:09:06 +0530

    Add abstract class Shape and Circle implementation

---
+++ abstractDemo.java
@@ -0,0 +1,25 @@
+import java.lang.*; 
+abstract class Shape 
+{ 
+ abstract void draw( ); 
+ void message( ) 
+ { 
+ System.out.println("This is a shape."); 
+ } 
+} 
+class Circle extends Shape 
+{ 
+ void draw( ) 
+ { 
+ System.out.println("Drawing Circle"); 
+ } 
+} 
+public abstractDemo
+{ 
+ public static void main(String[ ] args) 
+ { 
+ Shape s = new Circle(); 
+ s.draw(); 
+ s.message(); 
+ } 
+}
